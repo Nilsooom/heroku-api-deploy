6e6873a66f12a47a3ed5272853c17a1a5e6e4cc5
Reunilson Barbosa Da Silva <112037735+Nilsooom@users.noreply.github.com>
2023-03-19 14:47:51 -0300

    Update rotas.js

--- src/rotas.js
+++ src/rotas.js
@@ -7,7 +7,7 @@ const verificaLogin = require('./filtros/verificaLogin');
 const rotas = express();
 
 // cadastro de usuario
-rotas.post('/usuarios', usuarios.cadastrarUsuario);
+rotas.post('https://git.heroku.com/api-reunilson-dev-jr.git/usuarios', usuarios.cadastrarUsuario);
 
 // login
 rotas.post('/login', login.login);
